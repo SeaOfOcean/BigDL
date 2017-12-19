1a527e36a3bc40332e0bc1d9b432f4803804753f
seaofocean <xianyan.jia@intel.com>
2017-12-19 14:24:35 +0800

    update

--- pyspark/bigdl/transform/vision/image.py
+++ pyspark/bigdl/transform/vision/image.py
@@ -251,7 +251,7 @@ class DistributedImageFrame(ImageFrame):
         get prediction rdd from ImageFrame
         """
         predicts = callBigDlFunc(self.bigdl_type, "distributedImageFrameToPredict", self.value, key)
-        return predicts.map(lambda predict: (predict[0], predict[1].to_ndarray()))
+        return predicts.map(lambda predict: (predict[0], predict[1].to_ndarray()) if predict[1] else (predict[0], None))
 
 
 class HFlip(FeatureTransformer):
